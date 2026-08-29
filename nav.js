1a5c334eb2d9ca7595dcf10cdd031d49d6688b9e
ritikesh5808-cell <ritikesh5808@gmail.com>
2026-08-29 16:53:55 +0530

    Delete nav.js

--- nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var toggle = document.querySelector(".menu-toggle");
-  var nav = document.querySelector(".main-nav");
-  if (toggle && nav) {
-    toggle.addEventListener("click", function () {
-      nav.classList.toggle("open");
-    });
-  }
-});
